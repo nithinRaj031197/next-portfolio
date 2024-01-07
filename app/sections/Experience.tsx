d1b247a6b1263c84f57e1809cb8a2e005b42aa64
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-01-07 12:41:03 +0530

    Updated my career

--- app/sections/Experience.tsx
+++ app/sections/Experience.tsx
@@ -22,25 +22,25 @@ const Experience = () => {
       end: "July 2023",
       shortDescription: [
         ` Developed and designed an innovative chatbot integrated into a learning application, facilitating independent access to educational resources for visually impaired individuals, resulting in a user base exceeding 20,000.`,
-        `Createdandmaintainedreusablesoftwarecomponentsthatwere leveraged across various projects, reducing development efforts by 50`,
+        `Created and maintained reusable software components that were leveraged across various projects, reducing development efforts by 50`,
         `Implemented accessibility enhancements that significantly enhanced the user experience for visually impaired individuals.`,
         `TechStack:React.js,Node.js,GitHub,MySQL,AWS.`,
       ],
     },
-    // {
-    //   name: "Ecorework",
-    //   shortName: "Ecorework",
-    //   role: "Freelancer",
-    //   start: "Jan 2023",
-    //   end: "Jan 2023",
-    //   shortDescription: [
-    //     `Assetze is a cloud-based IT asset management solution that helps businesses generate
-    //     reports, manage IT expenses, and assign tickets on a unified platform.`,
-    //     `Developed APIs, integrated a vendor registration form, incorporated
-    //     a Donut Cart for scheduling employee shifts, and enhanced the frontend to ensure a responsive and
-    //     user-friendly interface.`,
-    //   ],
-    // },
+    {
+      name: "Ecorework",
+      shortName: "Ecorework",
+      role: "Freelancer",
+      start: "Jan 2023",
+      end: "Jan 2023",
+      shortDescription: [
+        `Assetze is a cloud-based IT asset management solution that helps businesses generate
+        reports, manage IT expenses, and assign tickets on a unified platform.`,
+        `Developed APIs, integrated a vendor registration form, incorporated
+        a Donut Cart for scheduling employee shifts, and enhanced the frontend to ensure a responsive and
+        user-friendly interface.`,
+      ],
+    },
     {
       name: "Exzatech Consulting Services Pvt Ltd.",
       shortName: "Exzatech",
@@ -57,18 +57,18 @@ const Experience = () => {
         `TechStack:React.js,Node.js,Nest.js,Angular,GitHub,PostgreSQL, Redux, MongoDB, MySQL`,
       ],
     },
-    // {
-    //   name: "Pixbit Solutions",
-    //   shortName: "Pixbit",
-    //   role: "ReactJs Developer (Trainee)",
-    //   start: "Aug 2021",
-    //   end: "Oct 2021",
-    //   shortDescription: [
-    //     "I received comprehensive training in ReactJs, Typescript, Material-UI 5, ReduxToolkit, and RTK Query.",
-    //     "I had the opportunity to contribute to a project named ”Infulls,”",
-    //     " I developed reusable Form Components, Autocomplete functionality, customized Accordion Components, and implemented Infinite Scrolling",
-    //   ],
-    // },
+    {
+      name: "Pixbit Solutions",
+      shortName: "Pixbit",
+      role: "ReactJs Developer (Trainee)",
+      start: "Aug 2021",
+      end: "Oct 2021",
+      shortDescription: [
+        "I received comprehensive training in ReactJs, Typescript, Material-UI 5, ReduxToolkit, and RTK Query.",
+        "I had the opportunity to contribute to a project named ”Infulls,”",
+        " I developed reusable Form Components, Autocomplete functionality, customized Accordion Components, and implemented Infinite Scrolling",
+      ],
+    },
   ];
   return (
     <motion.section
