e42ef0c935b3e33716a0cd173459eb7628c6a7bf
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-08-26 19:49:17 +0530

    Build fix

--- app/api/submitForm/route.ts
+++ app/api/submitForm/route.ts
@@ -1,37 +1,40 @@
 import { google } from "googleapis";
-import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { join } from "path";
-import { NextRequest } from "next/server";
 
 const sheets = google.sheets("v4");
 const spreadsheetId = "1IXvHEwo9hEzOJyWUHsUtixsZN6sjXOXCcsgjg6q570Q";
 
-export const POST = async (req: NextRequest) => {
-  try {
-    const formData = await req.json();
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      const formData = req.body;
 
-    const auth = new google.auth.GoogleAuth({
-      keyFile: join(process.cwd(), "app", "utils", "credentials.json"),
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
+      const auth = new google.auth.GoogleAuth({
+        keyFile: join(process.cwd(), "app", "utils", "credentials.json"),
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
 
-    const sheetsAPI = google.sheets({ version: "v4", auth });
+      const sheetsAPI = google.sheets({ version: "v4", auth });
 
-    const response = await sheetsAPI.spreadsheets.values.append({
-      spreadsheetId,
-      range: "Sheet1",
-      insertDataOption: "INSERT_ROWS",
-      requestBody: {
-        values: [[formData.name, formData.email, formData.message]],
-      },
-    });
+      const response = await sheetsAPI.spreadsheets.values.append({
+        spreadsheetId,
+        range: "Sheet1",
+        insertDataOption: "INSERT_ROWS",
+        requestBody: {
+          values: [[formData.name, formData.email, formData.message]],
+        },
+      });
 
-    return NextResponse.json({
-      message: "Form submitted successfully",
-      response: response.data,
-    });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ message: "Error submitting form", error });
+      res.status(200).json({
+        message: "Form submitted successfully",
+        response: response.data,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error submitting form", error });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
   }
-};
+}
