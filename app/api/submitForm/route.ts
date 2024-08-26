bc83cf29d3294508c70430a261f56a2d0a43baf6
nithinRaj031197 <nithinraj.official3@gmail.com>
2024-08-26 19:39:15 +0530

    Build fix

--- app/api/submitForm/route.ts
+++ app/api/submitForm/route.ts
@@ -1,40 +1,37 @@
-import { NextApiResponse } from "next";
 import { google } from "googleapis";
 import { NextResponse } from "next/server";
 import { join } from "path";
+import { NextRequest } from "next/server";
 
 const sheets = google.sheets("v4");
 const spreadsheetId = "1IXvHEwo9hEzOJyWUHsUtixsZN6sjXOXCcsgjg6q570Q";
-export const POST = async (req: any, res: NextApiResponse) => {
-  console.log(req.body);
-  if (req.method === "POST") {
-    const formData = req.body;
 
-    try {
-      const auth = new google.auth.GoogleAuth({
-        keyFile: join(process.cwd(), "app", "utils", "credentials.json"),
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
+export const POST = async (req: NextRequest) => {
+  try {
+    const formData = await req.json();
 
-      const sheetsAPI = google.sheets({ version: "v4", auth });
+    const auth = new google.auth.GoogleAuth({
+      keyFile: join(process.cwd(), "app", "utils", "credentials.json"),
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
 
-      const response = await sheetsAPI.spreadsheets.values.append({
-        spreadsheetId,
-        range: "Sheet1",
-        insertDataOption: "INSERT_ROWS",
-        requestBody: {
-          values: [[formData.name, formData.email, formData.message]],
-        },
-      });
-      return NextResponse.json({ message: "Form submitted successfully" });
-      res.status(200).json({ message: "Form submitted successfully", response: response.data });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ message: "Error submitting form", error });
-      res.status(500).json({ message: "Error submitting form" });
-    }
-  } else {
-    return NextResponse.json({ message: "Method Not Allowed" });
-    res.status(405).json({ message: "Method Not Allowed" });
+    const sheetsAPI = google.sheets({ version: "v4", auth });
+
+    const response = await sheetsAPI.spreadsheets.values.append({
+      spreadsheetId,
+      range: "Sheet1",
+      insertDataOption: "INSERT_ROWS",
+      requestBody: {
+        values: [[formData.name, formData.email, formData.message]],
+      },
+    });
+
+    return NextResponse.json({
+      message: "Form submitted successfully",
+      response: response.data,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Error submitting form", error });
   }
 };
