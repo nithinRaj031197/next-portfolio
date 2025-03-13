20900812b11e4dafe4ff9fdb99b69e1bc71980c6
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-13 21:17:44 +0530

    Initialized new portfolio in new-portfolio branch

--- app/canvas/index.ts
+++
@@ -1,5 +0,0 @@
-import EarthCanvas from "./Earth";
-import CanvasLoader from "./CanvasLoader";
-import StarsCanvas from "./StarsCanvas";
-
-export { EarthCanvas, CanvasLoader, StarsCanvas };
--- global.d.ts
+++
@@ -1,8 +0,0 @@
-export type Project = {
-  id: number;
-  title: string;
-  imagePath: string;
-  description?: string;
-  githubPath?: string;
-  projectPath?: string;
-};
