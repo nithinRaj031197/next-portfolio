5c2939c3a824c41084f6cb33b22a373971c2e35e
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-13 11:19:26 +0530

    feat: Social Links

--- components/ClientWrapper.jsx
+++ components/ClientWrapper.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import LogoLoader from "@/components/LogoLoader";
 import Header from "@/components/Header";
+import "@/styles/main.css";
 
 export default function ClientWrapper({ children }) {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,7 +17,12 @@ export default function ClientWrapper({ children }) {
       )}
       {isLoaded && (
         <>
-          <main className="mt-16">{children}</main>
+          <div className="video-container">
+            <video autoPlay loop muted className="background-video">
+              <source src="/northern_lights_bg.mp4" type="video/mp4" />
+            </video>
+            <main className="mt-16 min-h-screen">{children}</main>
+          </div>
         </>
       )}
     </>
